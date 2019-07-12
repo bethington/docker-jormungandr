b70092e66c5818b48459852977e0bf3041c8dd73
Benjamin Ethington <benaminde@gmail.com>
2019-07-11 21:09:46 -0600

    Change config generation to be in $HOME/data

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd $HOME/data
 bootstrap > bootstrap.txt
 cat bootstrap.txt
-jormungandr --genesis-block ./data/block-0.bin --config ./config.yaml --secret ./pool-secret1.yaml
+jormungandr --genesis-block ./block-0.bin --config ./config.yaml --secret ./pool-secret1.yaml
