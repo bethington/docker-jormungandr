5333d995fa28319a039d08d1e478e0430624f35b
Benjamin Ethington <benaminde@gmail.com>
2019-07-11 22:58:08 -0600

    Added search and replace to replace 127.0.0.1 to jormungandr

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 cd $HOME/data
 bootstrap > bootstrap.txt
 cat bootstrap.txt
+sed "s/127.0.0.1/jormungandr/g" config.yaml
 jormungandr --genesis-block ./block-0.bin --config ./config.yaml --secret ./pool-secret1.yaml
