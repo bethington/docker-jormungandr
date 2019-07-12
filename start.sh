388fe65abdf48d555579d06902c999f7095c3774
Benjamin Ethington <benaminde@gmail.com>
2019-07-11 23:17:41 -0600

    Edit localhost to be hostname

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@
 cd $HOME/data
 bootstrap > bootstrap.txt
 cat bootstrap.txt
-sed "s/127.0.0.1/jormungandr/g" config.yaml
+mv config.yaml config.yaml.example
+sed "s/127.0.0.1/jormungandr/g" config.yaml.example > config.yaml
 jormungandr --genesis-block ./block-0.bin --config ./config.yaml --secret ./pool-secret1.yaml
