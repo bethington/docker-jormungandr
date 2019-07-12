51bbd545f1cf3991d6bcfb69d324609ce9d55df0
Benjamin Ethington <benaminde@gmail.com>
2019-07-11 23:39:51 -0600

    Needs to be set to the IP of the host

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+HOST_IP="$(dig +short jormungandr)"
 cd $HOME/data
 bootstrap > bootstrap.txt
 cat bootstrap.txt
 mv config.yaml config.yaml.example
-sed "s/127.0.0.1/jormungandr/g" config.yaml.example > config.yaml
+sed "s/127.0.0.1/${HOST_IP}/g" config.yaml.example > config.yaml
 jormungandr --genesis-block ./block-0.bin --config ./config.yaml --secret ./pool-secret1.yaml
