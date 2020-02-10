f0e8e4da17f31558f39cdc233ff01fcd84393f85
Benjamin Ethington <benaminde@gmail.com>
2020-02-10 03:44:21 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker build -t bethington/jormungandr:v0.8.9 --build-arg BUILD=true --build-arg VER=v0.8.9 ./git/docker/.
+docker run -v ~/docker/jormungandr/cfg:/app/cfg -v ~/docker/jormungandr/secret:/app/secret -v ~/docker/jormungandr/data:/app/bin/data -it bethington/jormungandr:v0.8.9 /bin/sh ./bootstrap -p 8448 -x -c /app/cfg -k /app/secret
